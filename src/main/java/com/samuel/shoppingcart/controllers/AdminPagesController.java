70dbc8bff85f17b63851864a02cb8da83bbbdc34
Samuel <samuelmwkim@gmail.com>
2020-10-08 17:12:03 +0900

    add function added

--- src/main/java/com/samuel/shoppingcart/controllers/AdminPagesController.java
+++ src/main/java/com/samuel/shoppingcart/controllers/AdminPagesController.java
@@ -3,11 +3,15 @@ package com.samuel.shoppingcart.controllers;
 import com.samuel.shoppingcart.models.PageRepository;
 import com.samuel.shoppingcart.models.data.Page;
 import java.util.List;
+import javax.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 // 해당 class 내의 method들은 /admin/pages로 시작하는 url로 요청됨
@@ -32,5 +36,41 @@ public class AdminPagesController {
   public String add(@ModelAttribute Page page) {
     // model.addAttribute("page", new Page());
     return "admin/pages/add";
+  }
+
+  @PostMapping("/add")
+  public String add(
+    @Valid Page page,
+    BindingResult bindingResult,
+    RedirectAttributes redirectAttributes,
+    Model model
+  ) {
+    if (bindingResult.hasErrors()) {
+      return "admin/pages/add";
+    }
+
+    redirectAttributes.addFlashAttribute("message", "Page added");
+    redirectAttributes.addFlashAttribute("alertClass", "alert-success");
+
+    String slug = page.getSlug() == ""
+      ? page.getTitle().toLowerCase().replace(" ", "-")
+      : page.getSlug().toLowerCase().replace(" ", "-");
+
+    Page slugExists = pageRepo.findBySlug(slug);
+
+    if (slugExists != null) {
+      redirectAttributes.addFlashAttribute(
+        "message",
+        "Slug exists, choose another"
+      );
+      redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
+    } else {
+      page.setSlug(slug);
+      page.setSorting(100);
+
+      pageRepo.save(page);
+    }
+
+    return "redirect:/admin/pages/add";
   }
 }
--- src/main/java/com/samuel/shoppingcart/models/PageRepository.java
+++ src/main/java/com/samuel/shoppingcart/models/PageRepository.java
@@ -4,4 +4,6 @@ import com.samuel.shoppingcart.models.data.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 // Page Entity (PK: int Id) 를 위한 repository
-public interface PageRepository extends JpaRepository<Page, Integer> {}
+public interface PageRepository extends JpaRepository<Page, Integer> {
+  Page findBySlug(String slug);
+}
