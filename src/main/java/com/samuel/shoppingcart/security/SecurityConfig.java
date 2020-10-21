0f5adf603d02a1ca2b155318c7f9bcf67a47e8e2
Samuel <samuelmwkim@gmail.com>
2020-10-21 11:04:47 +0900

    adding User and Admin Entities

--- src/main/java/com/samuel/shoppingcart/security/SecurityConfig.java
+++ src/main/java/com/samuel/shoppingcart/security/SecurityConfig.java
@@ -9,17 +9,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    // http
-    //   .authorizeRequests()
-    //   .antMatchers("/")
-    //   .permitAll()
-    //   .antMatchers("/**")
-    //   .hasAnyRole("USER");
     http
       .authorizeRequests()
-      .antMatchers("/category/**")
-      .access("hasRole('ROLE_USER')")
       .antMatchers("/")
-      .access("permitAll");
+      .permitAll()
+      .antMatchers("/**")
+      .hasAnyRole("USER");
+    // http
+    //   .authorizeRequests()
+    //   .antMatchers("/category/**")
+    //   .access("hasRole('ROLE_USER')")
+    //   .antMatchers("/")
+    //   .access("permitAll");
   }
 }
