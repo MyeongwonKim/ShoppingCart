f7fdf9524aca88f85a79491b449b211c83f63de8
Samuel <samuelmwkim@gmail.com>
2020-10-21 23:17:11 +0900

    delicate touches

--- src/main/java/com/samuel/shoppingcart/security/RegistrationController.java
+++ src/main/java/com/samuel/shoppingcart/security/RegistrationController.java
@@ -8,7 +8,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,12 +35,12 @@ public class RegistrationController {
     BindingResult bindingResult,
     Model model
   ) {
-    if (validateUsernameDuplication(user.getUsername())) {
-      model.addAttribute("usernameDuplication", "Username already exists!");
+    if (bindingResult.hasErrors()) {
       return "register";
     }
 
-    if (bindingResult.hasErrors()) {
+    if (validateUsernameDuplication(user.getUsername())) {
+      model.addAttribute("usernameDuplication", "Username already exists!");
       return "register";
     }
 
